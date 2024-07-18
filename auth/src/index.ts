f1f9e602f54d5934b1dd1331feb25b29f5b104f9
kiran76 <ksknew76@gmail.com>
2024-07-18 18:48:01 +0530

    Scaffolding the routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,25 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { signinRouter } from "./routes/sign-in";
+import { signoutRouter } from "./routes/sing-out";
+import { signupRouter } from "./routes/sign-up";
+
 const app = express()
 const port = 4000;
 app.use(json())
 
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
+
+app.get("/", (req, res)=>{
+    res.send("zak zak zak")
+})
+
+app.get('/api/users/currentuser', (req, res)=>{
+    res.send('Hi there')
+})
 
 // port doesnt really matter when we use kubernetes cluster
 app.listen(port, ()=>{
