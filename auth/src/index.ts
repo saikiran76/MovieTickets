c77917826109cc5b875237102d0e66be592ca25b
kiran76 <ksknew76@gmail.com>
2024-07-18 15:10:33 +0530

    skaffold setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,10 +2,11 @@ import express from "express";
 import { json } from "body-parser";
 
 const app = express()
+const port = 4000;
 app.use(json())
 
 
 // port doesnt really matter when we use kubernetes cluster
-app.listen(4000, ()=>{
-    console.log("Auth service started at port 4000")
+app.listen(port, ()=>{
+    console.log(`Auth service started at port ${port}`)
 })
